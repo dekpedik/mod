ee8cd56a69064d1ced719cd02367534c596a0054
dekpedik <edeteee00@gmail.com>
2026-09-20 15:14:05 +0300

    Create StaminaAddon.java

---
+++ src/main/java/com/addon/stamina/StaminaAddon.java
@@ -0,0 +1,20 @@
+package com.addon.stamina;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(StaminaAddon.MOD_ID)
+public class StaminaAddon {
+    public static final String MOD_ID = "staminaaddon";
+
+    public StaminaAddon() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // Инициализация событий мода
+    }
+}
