36599c160c91304a02ddb67dcd3dbf51755ffa74
dekpedik <edeteee00@gmail.com>
2026-09-20 19:03:52 +0300

    Create PlayerMixin.java

---
+++ src/main/java/com/yourname/staminaaddon/mixin/PlayerMixin.java
@@ -0,0 +1,15 @@
+package com.yourname.staminaaddon.mixin;
+
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Player.class)
+public abstract class PlayerMixin {
+    @Inject(method = "hasEnoughFoodToStartSprinting", at = @At("HEAD"), cancellable = true)
+    private void alwaysAllowSprint(CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(true);
+    }
+}
